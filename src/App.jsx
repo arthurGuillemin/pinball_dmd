a3e4c75384b89ca986202f4e0fb8cb39c9ce4063
Younes Benaggoun <younes.benaggoun3@gmail.com>
2026-06-18 12:04:30 +0200

    feat(useGameState/app.jsx): add lives to dmd

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,27 @@ import { DMD_MESSAGES, getDMDRandomMessage } from "./data/messages";
 import { useGameState } from "./hooks/useGameState";
 
 function App() {
-  let { screen, score } = useGameState();
+  let { screen, score, balls } = useGameState();
 
   return (
     <main className="app">
-      {screen === "default" && <DMD text={score} scroll={false} />}
-      {screen === "gameOver" && <DMD text={DMD_MESSAGES.gameOver} />}
-      {screen === "pressStart" && <DMD text={DMD_MESSAGES.pressStart} />}
-      {screen === "bravo" && <DMD text={DMD_MESSAGES.bravo} scroll={false} />}
-      {screen === "go" && <DMD text={DMD_MESSAGES.go} scroll={false} />}
+      {screen === "default" && (
+        <DMD text={score} scroll={false} lives={balls} />
+      )}
+      {screen === "gameOver" && <DMD text={DMD_MESSAGES.gameOver} lives={0} />}
+      {screen === "pressStart" && (
+        <DMD text={DMD_MESSAGES.pressStart} lives={0} />
+      )}
+      {screen === "bravo" && (
+        <DMD text={DMD_MESSAGES.bravo} scroll={false} lives={balls} />
+      )}
+      {screen === "go" && (
+        <DMD text={DMD_MESSAGES.go} scroll={false} lives={0} />
+      )}
       {screen === "ball_lost" && (
-        <DMD text={getDMDRandomMessage("ball_lost")} scroll={false} />
-      )} 
-      <DMD text={DMD_MESSAGES.pressStart} lives={2} />
+        <DMD text={getDMDRandomMessage("ball_lost")} scroll={false} lives={0} />
+      )}
+      {/* <DMD text={DMD_MESSAGES.pressStart} lives={2} /> */}
     </main>
   );
 }
